4f26f884e3a30917b3aa757805ed756e1b035c60
Witoo Harianto <witoo@outlook.com>
2015-02-27 10:34:55 +0700

    update

--- errors2/check.go
+++ errors2/check.go
@@ -8,6 +8,11 @@ func ToError2(err error) *Errors2 {
 	return nil
 }
 
+func RespError(err error) (int, string) {
+	e := ToError2(err)
+	return e.code, e.message
+}
+
 func IsNotFound(err error) bool {
 	if e := ToError2(err); e != nil {
 		return e.ty == NotFound
