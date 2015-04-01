876e2f317f7870a4e33afc6d42b20c92dba12a1a
Witoo Harianto <witoo@outlook.com>
2015-04-01 14:54:06 +0700

    update

--- strings2/string2.go
+++ strings2/string2.go
@@ -1,33 +1,43 @@
 package string2
 
 import (
-  "reflect"
-  "unsafe"
+	"bytes"
+	"reflect"
+	"unsafe"
 )
 
 func BytesToString(b []byte) string {
-  bh := (*reflect.SliceHeader)(unsafe.Pointer(&b))
-  sh := reflect.StringHeader{bh.Data, bh.Len}
-  return *(*string)(unsafe.Pointer(&sh))
+	bh := (*reflect.SliceHeader)(unsafe.Pointer(&b))
+	sh := reflect.StringHeader{bh.Data, bh.Len}
+	return *(*string)(unsafe.Pointer(&sh))
 }
 
 func StringToBytes(s string) []byte {
-  sh := (*reflect.StringHeader)(unsafe.Pointer(&s))
-  bh := reflect.SliceHeader{sh.Data, sh.Len, 0}
-  return *(*[]byte)(unsafe.Pointer(&bh))
+	sh := (*reflect.StringHeader)(unsafe.Pointer(&s))
+	bh := reflect.SliceHeader{sh.Data, sh.Len, 0}
+	return *(*[]byte)(unsafe.Pointer(&bh))
 }
 
+// func Concat(s ...string) string {
+//   size := 0
+//   for i := 0; i < len(s); i++ {
+//     size += len(s[i])
+//   }
+
+//   buf := make([]byte, 0, size)
+
+//   for i := 0; i < len(s); i++ {
+//     buf = append(buf, StringToBytes(s[i])...)
+//   }
+
+//   return BytesToString(buf)
+// }
+
 func Concat(s ...string) string {
-  size := 0
-  for i := 0; i < len(s); i++ {
-    size += len(s[i])
-  }
+	buf := &bytes.Buffer{}
+	for i := 0; i < len(s); i++ {
+		buf.WriteString(s[i])
+	}
 
-  buf := make([]byte, 0, size)
-
-  for i := 0; i < len(s); i++ {
-    buf = append(buf, StringToBytes(s[i])...)
-  }
-
-  return BytesToString(buf)
+	return buf.String()
 }
