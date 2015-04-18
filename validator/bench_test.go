e9b6d4f6299f68b8096e76905355969ffd97298d
Witoo Harianto <witoo@outlook.com>
2015-04-18 19:48:57 +0700

    update

--- validator/bench_test.go
+++ validator/bench_test.go
@@ -13,6 +13,7 @@ func BenchmarkRequire(b *testing.B) {
 	for i := 0; i < b.N; i++ {
 		v := NewCheck()
 		v.RequiredString("val", errTest)
+		v.GetError()
 	}
 }
 
@@ -22,6 +23,7 @@ func BenchmarkMinChar(b *testing.B) {
 	for i := 0; i < b.N; i++ {
 		v := NewCheck()
 		v.MinChar("s", 2, errTest)
+		v.GetError()
 	}
 }
 
@@ -31,6 +33,7 @@ func BenchmarkEmail(b *testing.B) {
 	for i := 0; i < b.N; i++ {
 		v := NewCheck()
 		v.Email("test@test.com", errTest)
+		v.GetError()
 	}
 }
 
@@ -40,6 +43,7 @@ func BenchmarkGender(b *testing.B) {
 	for i := 0; i < b.N; i++ {
 		v := NewCheck()
 		v.Gender("test", errTest)
+		v.GetError()
 	}
 }
 
@@ -49,6 +53,7 @@ func BenchmarkConfirm(b *testing.B) {
 	for i := 0; i < b.N; i++ {
 		v := NewCheck()
 		v.Confirm("123", "321", errTest)
+		v.GetError()
 	}
 }
 
@@ -58,6 +63,7 @@ func BenchmarkISO8601DataTime(b *testing.B) {
 	for i := 0; i < b.N; i++ {
 		v := NewCheck()
 		v.ISO8601DataTime(`2014-04-18T10:47:23+07:00`, errTest)
+		v.GetError()
 	}
 }
 
@@ -67,5 +73,6 @@ func BenchmarkInString(b *testing.B) {
 	for i := 0; i < b.N; i++ {
 		v := NewCheck()
 		v.InString("222", []string{"1", "2", "3", "4"}, errTest)
+		v.GetError()
 	}
 }
